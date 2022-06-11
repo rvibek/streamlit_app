f7dad810c5821121704771a7a3c57a506c0e8826
rvibek <rvibek@gmail.com>
2022-06-11 17:09:56 +0000

    init

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+import requests
+import json
+import plotly.express as px
+
+
+st.title('Streamlit Dashboard')
+st.markdown('This is a Streamlit dashboard')
+
+st.subheader('Streamlit Dashboard')
+
+st.write('This is a Streamlit dashboard')
